a063bbfbdfbc7ba1f10676c033e104d7fd2e6c5c
Ana Beatriz <130497945+bsilvarm@users.noreply.github.com>
2023-08-12 19:26:23 -0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Document</title>
+</head>
+
+<body>
+<div class="container">
+    <div class="botoes">
+    <button class="button"><a href="../sisaluno/aluno/cadaluno.php">Cadastrar Aluno</a></button>
+    <button class="button"><a href="../sisaluno/professor/cadprof.php">Cadastrar Professor</a></button>
+    <button class="button"><a href="../sisaluno/disciplina/caddisc.php">Cadastrar Disciplina</a></button>
+    <button class="button"><a href="../sisaluno/aluno/listaluno.php">Lista Aluno</a></button>
+    <button class="button"><a href="../sisaluno/professor/listprof.php">Lista Professor</a></button>
+    <button class="button"><a href="../sisaluno/disciplina/listdisc.php">Lista Disciplina</a></button>
+    </div>
+    </div>
+
+</body>
+</html>
